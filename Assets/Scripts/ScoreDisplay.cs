4bf1e9e0d8d6992b5338ff17ae4482e05286d5bb
Bachmann <thierry.bachmann@free.fr>
2025-07-09 18:34:11 +0200

    Change display color when not in the correct direction

--- Assets/Scripts/ScoreDisplay.cs
+++ Assets/Scripts/ScoreDisplay.cs
@@ -7,10 +7,21 @@ public class ScoreDisplay : MonoBehaviour
     public PlayerController player;
     public TextMeshProUGUI scoreText;
     public GoalHandler goalHandler;
+    Color scoreGrowing;
+    Color scoreDecrease;
+    float lastScore;
 
+    void Start()
+    {
+        ColorUtility.TryParseHtmlString("#00F20B", out scoreGrowing);
+        ColorUtility.TryParseHtmlString("#FF7D88", out scoreDecrease);
+    }
     void Update()
     {
         if (scoreManager)
-            scoreText.text = $"Distance:{goalHandler.distance:N0} Score:{scoreManager.score:N0} Speed:{player.GetSpeed()*10f:N0} Multiplier:{player.speedMultiplier*10f:N0} {player.goalHandler.goalDirection:F2} {player.goalHandler.goalAngle:F2}"; 
+            scoreText.text = $"Distance:{goalHandler.distance:N0} Score:{scoreManager.score:N0} Speed:{player.GetSpeed() * 10f:N0} Multiplier:{player.speedMultiplier * 10f:N0} {player.goalHandler.goalDirection:F2} {player.goalHandler.goalAngle:F2}";
+        if (scoreManager.score > lastScore) scoreText.color = scoreGrowing;
+        if (scoreManager.score < lastScore) scoreText.color = scoreDecrease;
+        lastScore = scoreManager.score;
     }
 }
