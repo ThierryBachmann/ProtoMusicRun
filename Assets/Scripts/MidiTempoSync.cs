154db539c1526b69b528fd272a700b4f03f8a5b8
Bachmann <thierry.bachmann@free.fr>
2025-07-19 18:06:18 +0200

    Add collision sound

--- Assets/Scripts/MidiTempoSync.cs
+++ Assets/Scripts/MidiTempoSync.cs
@@ -5,16 +5,26 @@ public class MidiTempoSync : MonoBehaviour
 {
     public MidiFilePlayer midiPlayer;
     public PlayerController player;
+    
+    [Header("Ration Speed player vs Speed Music")]
+    public float RatioSpeedMusic = 0.4f;
+    public float MaxSpeedMusic = 3f;
+    
     private float previousSpeed = -1;
 
     void Update()
     {
-        float speed = Mathf.Clamp(player.GetSpeed() / (player.initialSpeed * 1f), 0.1f, 3.5f);
-        if (previousSpeed < 0f || Mathf.Abs(previousSpeed - speed) > 0.1f)
+        //   GetSpeed=initialSpeed * speedMultiplier
+        //   initialSpeed=5 (constant)
+        //   speedMultiplier 0.5 --> 10
+        //float speed = player.GetSpeed() / (player.initialSpeed * 1f);
+        float speed = player.speedMultiplier * 0.5f;
+        float speedClamp = Mathf.Clamp(speed, 0.1f, MaxSpeedMusic);
+        if (previousSpeed < 0f || Mathf.Abs(previousSpeed - speedClamp) > 0.2f)
         {
-            Debug.Log($"music speed {speed}");
-            midiPlayer.MPTK_Speed = speed;
-            previousSpeed=speed;
+            Debug.Log($"player.speedMultiplier: {player.speedMultiplier} music speed {speedClamp}");
+            midiPlayer.MPTK_Speed = speedClamp;
+            previousSpeed=speedClamp;
         }
     }
 }
--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,22 +1,89 @@
 using MidiPlayerTK;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem.XR;
 
 public class SoundManager : MonoBehaviour
 {
     public MidiStreamPlayer MidiSound;
-
-    public MPTKEvent SoundCollision1;
+    public List<Jingle> Jingles;
+    private Dictionary<string, List<SoundEvent>> jingleDict;
 
     void Start()
     {
-        SoundCollision1 = new MPTKEvent() { Command = MPTKCommand.NoteOn, Channel = 9, Duration = 1000, Value = 60 };
+        jingleDict = new Dictionary<string, List<SoundEvent>>();
+        foreach (var jingle in Jingles)
+        {
+            foreach (SoundEvent soundEvent in jingle.soundEvents)
+                soundEvent.BuildMPTKEvent();
+            jingleDict.Add(jingle.name, jingle.soundEvents);
+        }
     }
     public void PlayCollisionSound()
     {
         if (MidiSound)
         {
-            MidiSound.MPTK_PlayDirectEvent(SoundCollision1);
+            List<SoundEvent> ev;
+            if (jingleDict.TryGetValue("Collision", out ev))
+                StartCoroutine(PlaySoundCoroutine(ev));
+            else
+                Debug.LogError("Collision not found");
+        }
+    }
+    public IEnumerator PlaySoundCoroutine(List<SoundEvent> sounds)
+    {
+        foreach (SoundEvent sound in sounds)
+        {
+            switch (sound.action)
+            {
+                case SoundEvent.Action.WAIT:
+                    yield return new WaitForSeconds(sound.duration / 1000f);
+                    break;
+                case SoundEvent.Action.NOTEON:
+                    MidiSound.MPTK_PlayDirectEvent(sound.mptkEvent);
+                    break;
+            }
         }
     }
 }
+
+
+[Serializable]
+public class Jingle
+{
+    public string name;
+    public List<SoundEvent> soundEvents;
+}
+
+
+[Serializable]
+public class SoundEvent
+{
+    public enum Action
+    {
+        WAIT,
+        NOTEON,
+        PRESET,
+    }
+    public MPTKEvent mptkEvent;
+    public Action action;
+    public int value;
+    public int channel;
+    public int duration;
+
+    public void BuildMPTKEvent()
+    {
+        switch (action)
+        {
+            case Action.NOTEON:
+                mptkEvent = new MPTKEvent() { Command = MPTKCommand.NoteOn, Channel = channel, Value = value, Duration = duration };
+                break;
+
+            case Action.PRESET:
+                mptkEvent = new MPTKEvent() { Command = MPTKCommand.PatchChange, Channel = channel, Value = value };
+                break;
+        }
+    }
+}
+
