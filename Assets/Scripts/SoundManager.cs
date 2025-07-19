7b2bb0da5aea48839a8c18c9a3d75bc4e8ba140a
Bachmann <thierry.bachmann@free.fr>
2025-07-19 18:52:15 +0200

    Play jingle, to be review

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -6,23 +6,51 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+
     public MidiStreamPlayer MidiSound;
+    public bool RebuildJingles;
     public List<Jingle> Jingles;
     private Dictionary<string, List<SoundEvent>> jingleDict;
 
     void Start()
+    {
+        BuildJingles();
+    }
+
+    private void BuildJingles()
     {
         jingleDict = new Dictionary<string, List<SoundEvent>>();
         foreach (var jingle in Jingles)
         {
             foreach (SoundEvent soundEvent in jingle.soundEvents)
                 soundEvent.BuildMPTKEvent();
+            Debug.Log($"Jingle {jingle.name} rebuild");
             jingleDict.Add(jingle.name, jingle.soundEvents);
         }
     }
+
     public void PlayCollisionSound()
     {
         StartCoroutine(PlaySoundCoroutine("Collision"));
+    }
+
+    private float timeInterval = 0.5f; // Time interval in seconds
+    private float timer = 0.0f;
+    public void Update()
+    {
+        // Increment the timer by the time since the last frame
+        timer += Time.deltaTime;
+
+        // Check if the timer has reached the specified interval
+        if (timer >= timeInterval)
+        {
+            if (RebuildJingles)
+            {
+                RebuildJingles = false;
+                BuildJingles();
+            }
+            timer = 0.0f;
+        }
     }
     public IEnumerator PlaySoundCoroutine(string name)
     {
@@ -66,16 +94,22 @@ public class SoundEvent
     }
     public MPTKEvent mptkEvent;
     public Action action;
+    [Range(0, 127)]
     public int value;
+    [Range(0, 127)]
     public int channel;
+    [Range(0, 10000)] 
     public int duration;
+    [Range(0, 127)]
+    public int velocity;
 
     public void BuildMPTKEvent()
     {
         switch (action)
         {
             case Action.NOTEON:
-                mptkEvent = new MPTKEvent() { Command = MPTKCommand.NoteOn, Channel = channel, Value = value, Duration = duration };
+                mptkEvent = new MPTKEvent() { Command = MPTKCommand.NoteOn, Channel = channel, 
+                    Value = value, Duration = duration, Velocity = velocity };
                 break;
 
             case Action.PRESET:
