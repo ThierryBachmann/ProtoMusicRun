e04f3a96760a66e8e8a715ae6af1bd5d5978d6b5
Bachmann <thierry.bachmann@free.fr>
2025-09-21 08:50:50 +0200

    Rename project

---
+++ Assets/MusicBabyDemo/Scripts/Firebase/FirebaseKeyGitIgnore.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+namespace MusicRun
+{
+    public partial class FirebaseKey
+    {
+        public FirebaseKey()
+        {
+            firebaseApiKey = "AIzaSyAjxUwK9iS7IBMhDk1LJ-gRzh7AZCBwIzA";// "AIzaSyADsdund2yX2Cco3v2fWquqGxtgmKWSt5w";
+            firebaseProjectId = "protorunmusic";
+        }
+    }
+}
